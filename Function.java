455945f46ee9a559493a6aba2305771c776e8bee
Guilherme <guilhermefeitosa66@gmail.com>
2015-06-22 19:13:21 -0300

    changed function layout

--- Function.java
+++ Function.java
@@ -14,6 +14,7 @@ public class Function extends JFrame
   private JButton f2;
   private JButton f3;
   private JButton f4;
+  private JButton f5;
 
   public Function(Main main)
   {
@@ -24,26 +25,29 @@ public class Function extends JFrame
     setResizable(false);
     getContentPane().setBackground(Style.BACKGROUND);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    GridLayout grid = new GridLayout(4,0);
+    GridLayout grid = new GridLayout(5,0);
     //setLayout(new BoxLayout(getContentPane(), BoxLayout.LINE_AXIS));
     setLayout(grid);
     setLocation(910, 180);
 
 
-    f1 = new JButton("Composição paralela");
-    f2 = new JButton("Trim");
-    f3 = new JButton("Produto");
-    f4 = new JButton("xxx");
+    f1 = new JButton("AFN >> AFD");
+    f2 = new JButton("TRIM");
+    f3 = new JButton("Composição paralela");
+    f4 = new JButton("Produto");
+    f5 = new JButton("Minimizar");
 
     f1.setBackground(Style.BUTTON_COLOR);
     f2.setBackground(Style.BUTTON_COLOR);
     f3.setBackground(Style.BUTTON_COLOR);
     f4.setBackground(Style.BUTTON_COLOR);
+    f5.setBackground(Style.BUTTON_COLOR);
 
     add(f1);
     add(f2);
     add(f3);
     add(f4);
+    add(f5);
 
     setVisible(true);
   }
